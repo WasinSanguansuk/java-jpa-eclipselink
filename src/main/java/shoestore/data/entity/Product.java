bf2ba5ffb2131b0f4ff6daab2534a98bb833ca4d
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-08-29 14:46:57 -0500

    Added Product entity

---
+++ src/main/java/shoestore/data/entity/Product.java
@@ -0,0 +1,96 @@
+package shoestore.data.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    // Data
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "itemId")
+    private Integer itemId;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "size")
+    private Double size;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "availability")
+    private Integer availability;
+
+    // Constructors
+    public Product() {
+    }
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Integer availability) {
+        this.availability = availability;
+    }
+
+    // Methods
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", itemId=" + itemId +
+                ", color='" + color + '\'' +
+                ", size=" + size +
+                ", price=" + price +
+                ", availability=" + availability +
+                '}';
+    }
+
+
+}
